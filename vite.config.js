2ec5b1d561208d10e52e5566fecddfdeeb935704
Thamires Febbo <thamiresfebbo@hotmail.com>
2026-05-08 18:03:05 -0300

    config deploy github pages

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  base: "https://thamiresfebbo.github.io/Hackathon-Artemisia-Elas-Tech-_-Front-end_GP02/",
+});
